97e93b6570a88e8faec68c05c2c7095a0b38c2f1
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-01-15 13:03:19 +0100

    dev: no more console.log

--- test/browser/dwg2json/dwgwrite.mjs
+++ test/browser/dwg2json/dwgwrite.mjs
@@ -1,22 +1,13 @@
 import createModuleInstance from "../../../dist/libredwgwrite.mjs";
-
 export const dwgwrite = async (/** @type {string} */ json) => {
-  console.log("json: ", json);
-
   const WASM_JSON_FILENAME = "tmp.json";
   const WASM_DWG_FILENAME = "tmp.dwg";
-
   const instance = await createModuleInstance({
     noInitialRun: true,
     printErr: () => {},
   });
-  console.log("instance created");
-
   instance.FS.writeFile(WASM_JSON_FILENAME, json);
-  console.log("json wasm file created");
-
   const main = instance.cwrap("main", "number", ["number", "number"]);
-
   const makeMainArgs = (instance, array) => {
     // ASSUMPTION: pointer size is 4 bytes
     const ptr = instance._malloc(array.length * 4);
@@ -38,11 +29,7 @@ export const dwgwrite = async (/** @type {string} */ json) => {
     WASM_JSON_FILENAME,
   ];
   const { argv, argc } = makeMainArgs(instance, args);
-
-  console.log("about to execute main");
   main(argc, argv);
-  console.log("main executed.");
-
   const buffer = instance.FS.readFile(WASM_DWG_FILENAME);
   console.log("buffer: ", buffer);
 
